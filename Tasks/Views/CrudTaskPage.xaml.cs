bf782f6e81c461acb13b1afd4ff25d8da6f448a8
Fábio Valentim <maitabom@gmail.com>
2024-12-07 15:02:39 -0300

    Implentação de ações de botões de inserção de tarefas

--- Tasks/Views/CrudTaskPage.xaml.cs
+++ Tasks/Views/CrudTaskPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class CrudTaskPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnClose_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PopModalAsync();
+    }
+
+    private void btnSalvar_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopModalAsync();
+    }
+
+    private async void btnAddSubtask_Clicked(object sender, EventArgs e)
+    {
+        var stepName = await DisplayPromptAsync("Etapa (Subtarefa)", "Digite o nome da etapa (subtarefa)", "Adicionar", "Cancelar");
+    }
 }
