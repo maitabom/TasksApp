4ef2523fa34de693189fb681dfaf5aca773a1dea
Fábio Valentim <maitabom@gmail.com>
2024-12-07 16:26:37 -0300

    Retirada de bordas dos componentes de texto

--- Tasks/App.xaml.cs
+++ Tasks/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Tasks.Views;
+﻿using Microsoft.Maui.Platform;
+using Tasks.Views;
 
 namespace Tasks
 {
@@ -6,9 +7,52 @@ namespace Tasks
     {
         public App()
         {
+            CustomHandler();
             InitializeComponent();
 
             MainPage = new NavigationPage(new StartPage());
+        }
+
+        private void CustomHandler()
+        {
+            EntryNoBorder();
+            DatePickerNoBorder();
+        }
+
+        private void EntryNoBorder()
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+#if ANDROID
+                //Android
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
+#elif IOS || MACCATALYST
+                //iOS & Mac
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //Windows
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
+        }
+
+        private void DatePickerNoBorder()
+        {
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+#if ANDROID
+                //Android
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
+#elif IOS || MACCATALYST
+                //iOS & Mac
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //Windows
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
         }
     }
 }
