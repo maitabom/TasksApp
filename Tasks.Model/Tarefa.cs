e8d47667509aa2d4ce46cf4ae84b359c549007b5
Fábio Valentim <maitabom@gmail.com>
2024-12-08 14:38:11 -0300

    Criação de biblioteca de classes de modelo de tarefas

---
+++ Tasks.Model/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Model;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = String.Empty;
+    public string Descricao { get; set; } = String.Empty;
+    public DateTime DataPrevisao { get; set; }
+    public bool Concluido { get; set; }
+    public List<Subtarefa> Subtarefas { get; set; } = new List<Subtarefa>();
+}
