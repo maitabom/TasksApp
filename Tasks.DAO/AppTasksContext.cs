f54b69b8b1c3087e9f2e8e679ab35c720e3c9128
Fábio Valentim <maitabom@gmail.com>
2024-12-08 15:46:13 -0300

    Criação do projeto de acesso ao banco de dados

---
+++ Tasks.DAO/AppTasksContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasks.Model;
+
+namespace Tasks.DAO;
+
+public class AppTasksContext : DbContext
+{
+    public DbSet<Tarefa> Tarefas { get; set; }
+    public DbSet<Subtarefa> Subtarefas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var databaseName = "apptask.db";
+        var databasePath = Path.Combine(folderPath, databaseName);
+        
+        optionsBuilder.UseSqlite(databasePath);
+    }
+}
