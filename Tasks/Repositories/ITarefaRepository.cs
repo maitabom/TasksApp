e66d9c72740f484a2478a0c2d1561626e180388f
Fábio Valentim <maitabom@gmail.com>
2024-12-14 17:39:48 -0300

    Criação de repositório de tarefas

---
+++ Tasks/Repositories/ITarefaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Model;
+
+namespace Tasks.Repositories;
+
+public interface ITarefaRepository
+{
+    IList<Tarefa> GetAll();
+    Tarefa Get(int id);
+    void Insert(Tarefa tarefa);
+    void Update(Tarefa tarefa);
+    Task Delete(int id);
+    void Delete(Tarefa tarefa);
+}
---
+++ Tasks/Repositories/TarefaRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Model;
+
+namespace Tasks.Repositories;
+
+public class TarefaRepository : ITarefaRepository
+{
+    public Task Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Tarefa tarefa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tarefa Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<Tarefa> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(Tarefa tarefa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Tarefa tarefa)
+    {
+        throw new NotImplementedException();
+    }
+}
