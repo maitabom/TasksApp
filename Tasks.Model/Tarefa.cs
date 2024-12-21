8e992aeb292d210ad170cd13143b64a174f51db8
Fábio Valentim <maitabom@gmail.com>
2024-12-21 12:58:18 -0300

    Implementação de mudança de eventos de classe modelo de tarefa

--- Tasks.Model/Tarefa.cs
+++ Tasks.Model/Tarefa.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Tasks.Model;
 
-public class Tarefa
+public class Tarefa : INotifyPropertyChanged
 {
+    private bool _concluido;
+
     public int Id { get; set; }
     public string Nome { get; set; } = String.Empty;
     public string Descricao { get; set; } = String.Empty;
     public DateTime DataPrevisao { get; set; }
-    public bool Concluido { get; set; }
+
+    public bool Concluido
+    {
+        get
+        {
+            return _concluido;
+        }
+        set
+        {
+            _concluido = value;
+            OnPropertyChanged(nameof(Concluido));
+        }
+    }
+
     public DateTime Criado { get; set; }
     public DateTime Atualizado { get; set; }
     public List<Subtarefa> Subtarefas { get; set; } = new List<Subtarefa>();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged is not null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
