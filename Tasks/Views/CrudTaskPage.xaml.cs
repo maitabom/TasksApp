536d5c284c6a71121b5581d4af62a3b2d22731f8
Fábio Valentim <maitabom@gmail.com>
2024-12-21 13:51:03 -0300

    Implementação de gestão de subtarefas

--- Tasks/Views/CrudTaskPage.xaml.cs
+++ Tasks/Views/CrudTaskPage.xaml.cs
@@ -1,15 +1,22 @@
+using Tasks.Model;
+
 namespace Tasks.Views;
 
 public partial class CrudTaskPage : ContentPage
 {
-	public CrudTaskPage()
-	{
-		InitializeComponent();
-	}
+    private Tarefa Tarefa { get; set; }
+
+    public CrudTaskPage()
+    {
+        InitializeComponent();
+        Tarefa = new Tarefa();
+
+        BindableLayout.SetItemsSource(slSubtarefas, Tarefa.Subtarefas);
+    }
 
     private void btnClose_Clicked(object sender, EventArgs e)
     {
-		Navigation.PopModalAsync();
+        Navigation.PopModalAsync();
     }
 
     private void btnSalvar_Clicked(object sender, EventArgs e)
@@ -20,11 +27,26 @@ public partial class CrudTaskPage : ContentPage
     private async void btnAddSubtask_Clicked(object sender, EventArgs e)
     {
         var stepName = await DisplayPromptAsync("Etapa (Subtarefa)", "Digite o nome da etapa (subtarefa)", "Adicionar", "Cancelar");
+
+        if (!String.IsNullOrEmpty(stepName))
+        {
+            Tarefa.Subtarefas.Add(new Subtarefa({ Nome = stepName, Concluido = false });
+        }
     }
 
     protected override void OnSizeAllocated(double width, double height)
     {
         base.OnSizeAllocated(width, height);
         dpTaskDate.WidthRequest = width - 30;
+    }
+
+    private void btnExcluirSubtarefa_Click(object sender, TappedEventArgs e)
+    {
+        var marcado = (Subtarefa?)e.Parameter;
+
+        if (marcado is not null)
+        {
+            Tarefa.Subtarefas.Remove(marcado);
+        }
     }
 }
