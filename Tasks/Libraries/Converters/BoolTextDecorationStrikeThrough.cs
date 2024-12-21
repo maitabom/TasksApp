3e71e613cc64dcb248188a1bd795db118e53db7a
Fábio Valentim <maitabom@gmail.com>
2024-12-21 12:58:36 -0300

    Criação do conversor de text decoration

---
+++ Tasks/Libraries/Converters/BoolTextDecorationStrikeThrough.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Libraries.Converters;
+
+public class BoolTextDecorationStrikeThrough : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not null)
+        {
+            bool completed = (bool)value;
+            return completed ? TextDecorations.Strikethrough : TextDecorations.None;
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
